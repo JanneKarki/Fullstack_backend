18e285087dd3808859ee11154c3fc87eeb2c6235
Janne Kärki <janne.karki@helsinki.fi>
2023-03-05 16:06:40 +0200

    Task 3.3 done

--- index.js
+++ index.js
@@ -34,7 +34,15 @@ app.get('/info', (request, response) => {
     response.send(`<div><p>Phonebook has info for ${count} people</p><p>${new Date()}</p></div>`)
   })
 
-
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {    
+        response.json(person)  
+    } else {    
+        response.status(404).end()  
+    }
+})
 
 
 const PORT = 3001
