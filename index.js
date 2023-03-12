b692f7d13f579b660366db0e47123da120b0513b
Janne Kärki <janne.karki@helsinki.fi>
2023-03-12 14:24:01 +0200

    Task 3.13 done

--- index.js
+++ index.js
@@ -1,60 +1,42 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
+
 const app = express()
 app.use(express.static('./backend/build'))
 app.use(express.json())
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-
-const cors = require('cors')
-
 app.use(cors())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body', {}
+))
 morgan.token('req-body', (req) => JSON.stringify(req.body))
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body', {
-  
-}))
-
 const generateId = () => {
-    const id = Math.floor(Math.random() * 100000);
-    return id + 1
+  const id = Math.floor(Math.random() * 100000);
+  return id
 }
 
+/*
 function searchName(name) {
-    for (let i = 0; i < persons.length; i++) { 
-        const person = persons[i];
+    Person.find({ name: name }).then(person => {
+      console.log(person)
         if (person.name === name) {
             return true;
         } 
-    }
-    return false;
+        return false;
+    })
 }
-
-  
-  app.post('/api/persons', (request, response) => {
+*/
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+})
+/*
+app.post('/api/persons', (request, response) => {
     const body = request.body
-    
+      
     if (!body.name) {
       return response.status(400).json({ 
         error: 'name missing' 
@@ -70,46 +52,43 @@ function searchName(name) {
             error: 'name already exists ' 
           })
     }
-    const person = {
+    const person = new Person ({
+      id: generateId(),
       name: body.name,
       number: body.number,
-      id: generateId(),
-    }
-  
-    persons = persons.concat(person)
-  
-    response.json(persons)
+    })
+    person.save().then(result => {
+      console.log(`added ${body.name} number ${body.number}`)
+    })
+    
   })
 
-
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
-
-  })
-
-app.get('/info', (request, response) => {
-    const count = persons.length;
-    response.send(`<div><p>Phonebook has info for ${count} people</p><p>${new Date()}</p></div>`)
-  })
-
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {    
-        response.json(person)  
-    } else {    
-        response.status(404).end()  
-    }
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
-})
-
-
+*/
 const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+//app.get('/info', (request, response) => {
+//    const count = persons.length;
+//    response.send(`<div><p>Phonebook has info for ${count} people</p><p>${new Date()}</p></div>`)
+//  })
+
+//app.get('/api/persons/:id', (request, response) => {
+//    const id = Number(request.params.id)
+//    const person = persons.find(person => person.id === id)
+ //   if (person) {    
+ //       response.json(person)  
+ //   } else {    
+ //       response.status(404).end()  
+ //   }
+//})
+
+//app.delete('/api/persons/:id', (request, response) => {
+ //   const id = Number(request.params.id)
+ //   persons = persons.filter(person => person.id !== id)
+ //   response.status(204).end()
+//})
+
+
+
