16bbe4dd180959f950617ba2c4bf65f9ca82b117
Janne Kärki <janne.karki@helsinki.fi>
2023-03-12 18:03:19 +0200

    Task 3.17 done

--- index.js
+++ index.js
@@ -80,8 +80,6 @@ app.post('/api/persons', (request, response) => {
     })
   })
 
-  
-
 
 
 //app.get('/info', (request, response) => {
@@ -100,6 +98,23 @@ app.get('/api/persons/:id', (request, response, next) => {
     })
     .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const id = Number(request.params.id)
+ console.log(`${request.params.id} ${body.name}`)
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
